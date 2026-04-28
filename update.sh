6c09e06d6cdada3a6d2c888db6ff6e666de0ecfd
Lucas Berrigan <leberrigan@gmail.com>
2026-04-28 12:42:00 -0300

    v0.2.7 Prevent updater from removing files created during install.

--- update.sh
+++ update.sh
@@ -35,35 +35,54 @@ update_repo() {
     echo "Updates found. Pulling changes..."
     git pull --ff-only origin "$branch"
     updates_applied=true
+    return 1
 }
 
 # 1 Update enviroPi repo
 if [ -d "$HOME_DIR/enviroPi/.git" ]; then
     update_repo "$HOME_DIR/enviroPi" "$REPO_BRANCH"
+    result=$?
 
-    if [ "$updates_applied" = true ]; then
+    if [ "$result" -eq 1 ]; then
         echo "Syncing enviroPi files..."
-        sudo rsync -av --delete "$HOME_DIR/enviroPi/" "$ENPI_DIR/"
+        OLD_REQ_HASH=$(sha256sum "$ENPI_DIR/requirements.txt" 2>/dev/null | awk '{print $1}')
+        sudo rsync -av --delete \
+          --exclude-from="$ENPI_DIR/.rsync-exclude" \
+          "$HOME_DIR/enviroPi/" "$ENPI_DIR/"
+
         sudo chown -R "$SERVICE_USER":"$SERVICE_USER" "$ENPI_DIR"
+        NEW_REQ_HASH=$(sha256sum "$ENPI_DIR/requirements.txt" 2>/dev/null | awk '{print $1}')
+
+        # Check to see if required packages have changed.
+        
+        if [ "$OLD_REQ_HASH" != "$NEW_REQ_HASH" ]; then
+          echo "requirements.txt changed — updating Python dependencies"
+          sudo -u "$SERVICE_USER" bash <<EOF
+            cd "$ENPI_DIR"
+            source env/bin/activate
+            pip install -r requirements.txt
+EOF
+        else
+          echo "requirements.txt unchanged — skipping pip install"
+        fi
     fi
 else
     echo "enviroPi repo not found, skipping."
 fi
 
 # 2 Update sg-control repo
+updates_applied=false
 if [ -d "$SG_REPO_DIR/.git" ]; then
     update_repo "$SG_REPO_DIR" "enpi"
+    result=$?
 
-    if [ "$updates_applied" = true ]; then
+    if [ "$result" -eq 1 ]; then
         echo "Updating sg-control files..."
         sudo cp "$SG_REPO_DIR/src/dashboard.js" "$SG_CONTROL_DIR/"
         sudo cp "$SG_REPO_DIR/src/enpi.js" "$SG_CONTROL_DIR/"
         sudo cp "$SG_REPO_DIR/src/fd-config.json" "$SG_CONTROL_DIR/"
         sudo cp "$SG_REPO_DIR/src/main.js" "$SG_CONTROL_DIR/"
         sudo cp "$SG_REPO_DIR/src/motus_up.js" "$SG_CONTROL_DIR/"
-
-        echo "Restarting sg-control service..."
-        sudo systemctl restart sg-control
     fi
 else
     echo "sg-control repo not found, skipping."
@@ -74,6 +93,9 @@ if [ "$updates_applied" = true ]; then
     echo
     echo "Restarting pigpio..."
     sudo systemctl restart pigpiod
+
+    echo "Restarting sg-control service..."
+    sudo systemctl restart sg-control
 else
     echo
     echo "No updates applied. No services restarted."
